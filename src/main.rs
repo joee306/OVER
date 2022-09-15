0c1d74e8e387ce35a166e3ae572d96f850f31520
joee306 <joeesser.dev@gmail.com>
2022-09-15 22:18:59 +0200

    14.9.22

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ impl Block {
 #[derive(Component)]
 struct Chunk {
     coords : (i64, i64, i64),
-    data : [[[Block ; CHUNK_DIMENSIONS.0] ; CHUNK_DIMENSIONS.2] ; CHUNK_DIMENSIONS.1],
+    data : [[[Block ; CHUNK_DIMENSIONS.0] ; CHUNK_DIMENSIONS.1] ; CHUNK_DIMENSIONS.2], //x [1] y [2] z [0] 
     loaded : bool,
 }
 
@@ -44,7 +44,7 @@ impl Chunk {
             [[[ Block {
                 render_side : LayerSide { XM : false , XP : false , YM : false , YP : false , ZM : false , ZP : false },
                 material : 0,
-            }; CHUNK_DIMENSIONS.0] ; CHUNK_DIMENSIONS.2] ; CHUNK_DIMENSIONS.1],
+            }; CHUNK_DIMENSIONS.0] ; CHUNK_DIMENSIONS.1] ; CHUNK_DIMENSIONS.2],
             loaded : false,
         }
     }
@@ -55,6 +55,57 @@ impl Chunk {
             return true;
         } else {
             false
+        }
+    }
+    pub fn calc_layers(&mut self) {
+        for x in 0..self.data.len() {
+            for y in 0..self.data[0].len() {
+                for z in 0..self.data[0][0].len(){
+
+                    // check for block in xpos //
+                    if x == self.data.len() {
+                        self.data[x][y][z].render_side.XP = true;
+                    } else if self.data[x + 1][y][z].material == 0 {
+                        self.data[x][y][z].render_side.XP = true;
+                    }
+                    /////////////////////////////
+                    // check for block in xmin //
+                    if x == 0 {
+                        self.data[x][y][z].render_side.XM = true;
+                    } else if self.data[x - 1][y][z].material == 0 {
+                        self.data[x][y][z].render_side.XM = true;
+                    }
+                    /////////////////////////////
+                    // check for block in ypos //
+                    if y == self.data[0].len() {
+                        self.data[x][y][z].render_side.YP = true;
+                    } else if self.data[x][y + 1][z].material == 0 {
+                        self.data[x][y][z].render_side.YP = true;
+                    }
+                    /////////////////////////////
+                    // check for block in ymin //
+                    if y == 0 {
+                        self.data[x][y][z].render_side.YM = true;
+                    } else if self.data[x][y - 1][z].material == 0 {
+                        self.data[x][y][z].render_side.YM = true;
+                    }
+                    /////////////////////////////
+                    // check for block in zpos //
+                    if x == self.data[0][0].len() {
+                        self.data[x][y][z].render_side.ZP = true;
+                    } else if self.data[x][y][z + 1].material == 0 {
+                        self.data[x][y][z].render_side.ZP = true;
+                    }
+                    /////////////////////////////
+                    // check for block in zmin //
+                    if x == 0 {
+                        self.data[x][y][z].render_side.ZM = true;
+                    } else if self.data[x][y][z - 1].material == 0 {
+                        self.data[x][y][z].render_side.ZM = true;
+                    }
+                    /////////////////////////////
+                }
+            }
         }
     }
 }
@@ -83,10 +134,11 @@ fn setup(
     mut commands: Commands,
 ) {
     commands.spawn().insert(Chunk::new(0,0,0));
-    commands.spawn().insert(Chunk::new(16,0,0));
+    //commands.spawn().insert(Chunk::new(16,0,0));
     commands.spawn_bundle(PointLightBundle {
         point_light: PointLight {
             intensity: 1500000.0,
+                    //println!("{} - {} - {}",chunk.coords.0,chunk.coords.2, chunk.data[0][0].len() );
             shadows_enabled: true,
             ..default()
         },
@@ -106,6 +158,7 @@ fn chunk_inicator(
     mut materials: ResMut<Assets<StandardMaterial>>,
     query : Query<&Chunk>,
 
+                    //println!("{} - {} - {}",chunk.coords.0,chunk.coords.2, chunk.data[0][0].len() );
 ) {
     for chunk in query.iter() {
         if chunk.loaded {
@@ -128,24 +181,25 @@ fn render(
 ) {
     for mut chunk in query.iter_mut() {
         if !chunk.loaded {
-            for x in chunk.coords.0 as usize..chunk.coords.0 as usize + chunk.data[0].len() {
-                for y in chunk.coords.1 as usize..chunk.coords.1 as usize + chunk.data.len() {
+            chunk.calc_layers();
+            for x in chunk.coords.0 as usize..chunk.coords.0 as usize + chunk.data.len() {
+                for y in chunk.coords.1 as usize..chunk.coords.1 as usize + chunk.data[0].len() {
                     for z in chunk.coords.2 as usize..chunk.coords.2 as usize + chunk.data[0][0].len() {
                         if chunk.get(x,y,z) {
                             let mut rng = rand::thread_rng();
-                            //println!("{:?}",chunk.data[x - 1][y - 1][z - 1].render_side);
-                            commands.spawn_bundle(PbrBundle {
-                                mesh: meshes.add(Mesh::from(shape::Plane { size: 1.0 })),
-                                //material: materials.add(Color::rgb(0.33, 0.49, 0.27).into()),
-                                material: materials.add(Color::rgb(rng.gen::<f32>(),rng.gen::<f32>(),rng.gen::<f32>()).into()),
-                                transform: Transform::from_xyz(x as f32 ,y as f32 ,z as f32),
-                                ..default()
-                            });
+                            if chunk.data[x][y][z].render_side.YP {
+                                commands.spawn_bundle(PbrBundle {
+                                    mesh: meshes.add(Mesh::from(shape::Plane { size: 1.0 })),
+                                    //material: materials.add(Color::rgb(0.33, 0.49, 0.27).into()),
+                                    material: materials.add(Color::rgb(rng.gen::<f32>(),rng.gen::<f32>(),rng.gen::<f32>()).into()),
+                                    transform: Transform::from_xyz(x as f32 ,y as f32 ,z as f32),
+                                    ..default()
+                                });
+                            }
                         }
                     }
                 }
             }
-            println!("chunk loaded with <{:?}> ", chunk.coords);
         }
         chunk.loaded = true;
     }
